94f23303910185d9a9f92e1fc164df5467378e8a
Colton Thompson <colton.thompson@lbaware.com>
2025-05-07 06:41:29 -0400

    Initial commit

---
+++ CT.Cli/Program.cs
@@ -0,0 +1,42 @@
+﻿// Program.cs
+using System;
+using System.CommandLine;
+
+namespace HelloWorldCLI
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // Create a root command
+            var rootCommand = new RootCommand("A simple hello world CLI application.");
+
+            // Add a name option
+            var nameOption = new Option<string>(
+                "--name",
+                description: "The name to greet."
+            )
+            {
+                IsRequired = false
+            };
+            nameOption.AddAlias("-n");
+            rootCommand.AddOption(nameOption);
+
+            // Set the handler
+            rootCommand.SetHandler((string name) =>
+            {
+                // Console.WriteLine("Hello World!");
+                if (!string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine($"Hello {name}!");
+                }
+                else{
+                    Console.WriteLine("Hello World!");
+                }
+            }, nameOption);
+
+            // Parse the command line arguments and invoke the handler
+            return rootCommand.Invoke(args);
+        }
+    }
+}
